848911efef245886f41a908692ca75cd31bfa922
Katja Roehlig <katja-roehlig@mail.de>
2023-06-20 14:09:18 +0100

    fix: delete redundant link

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -26,12 +26,6 @@ import Github from "./icons/Github.vue";
         >
           <Xing class="contact__icon" />Xing</a
         >
-        <a
-          href="https://www.xing.com/profile/Katja_Roehlig/cv"
-          class="contact__link"
-        >
-          <Xing class="contact__icon" />Xing</a
-        >
 
         <a href="https://github.com/katja-roehlig" class="contact__link">
           <Github class="contact__icon" />Github</a
